90742b28043c6eb89aa36978629820e0ef92f261
yanchen01 <ychen01@bu.edu>
2021-05-17 13:19:20 -0400

    refatoring frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './App.css';
 // importing components
-import Form from "./components/Form";
+import Form from './components/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> drugML</h1>
-      </header>
-      <Form />
-    </div>
-  );
+	return (
+		<div className="App">
+			<ChakraProvider>
+				<h2>Drug ML</h2>
+			</ChakraProvider>
+		</div>
+	);
 }
 
 export default App;
